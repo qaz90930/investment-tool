e2ce12588e89bce2a517ccfbf66281da49e6a36f
Hank Chou <hank@sharesies.co.nz>
2019-05-30 17:05:49 +1200

    link to local db

--- main.go
+++ main.go
@@ -1,74 +1,28 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 type BitcoinPrice struct {
-	Ask  float64 `json:"ask"`
-	Bid  float64 `json:"bid"`
-	Last float64 `json:"last"`
-	High float64 `json:"high"`
-	Low  float64 `json:"low"`
-	Open struct {
-		Hour   float64 `json:"hour"`
-		Day    float64 `json:"day"`
-		Week   float64 `json:"week"`
-		Month  float64 `json:"month"`
-		Month3 float64 `json:"month_3"`
-		Month6 float64 `json:"month_6"`
-		Year   float64 `json:"year"`
-	} `json:"open"`
-	Averages struct {
-		Day   float64 `json:"day"`
-		Week  float64 `json:"week"`
-		Month float64 `json:"month"`
-	} `json:"averages"`
-	Volume  float64 `json:"volume"`
-	Changes struct {
-		Percent struct {
-			Hour   float64 `json:"hour"`
-			Day    float64 `json:"day"`
-			Week   float64 `json:"week"`
-			Month  float64 `json:"month"`
-			Month3 float64 `json:"month_3"`
-			Month6 float64 `json:"month_6"`
-			Year   float64 `json:"year"`
-		} `json:"percent"`
-		Price struct {
-			Hour   float64 `json:"hour"`
-			Day    float64 `json:"day"`
-			Week   float64 `json:"week"`
-			Month  float64 `json:"month"`
-			Month3 float64 `json:"month_3"`
-			Month6 float64 `json:"month_6"`
-			Year   float64 `json:"year"`
-		} `json:"price"`
-	} `json:"changes"`
-	VolumePercent    float64 `json:"volume_percent"`
-	Timestamp        int     `json:"timestamp"`
-	DisplayTimestamp string  `json:"display_timestamp"`
-	DisplaySymbol    string  `json:"display_symbol"`
+	gorm.Model
+	Price string
+	// Date  *time.Time
 }
 
 func Bitcoin(c *gin.Context) {
-	respone, err := http.Get("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD")
+	db, err := gorm.Open("postgres", "host=localhost dbname=app_db")
 	if err != nil {
-		fmt.Println("Error:", err)
-	} else {
-		data, _ := ioutil.ReadAll(respone.Body)
-		var bp BitcoinPrice
-		json.Unmarshal([]byte(data), &bp)
-		c.JSON(200, gin.H{
-			"price": bp.Last,
-		})
-		fmt.Println(bp.Last)
+		panic("failed to connect the DB")
 	}
+	defer db.Close()
+	var price BitcoinPrice
+	data := db.First(price)
+	fmt.Printf("%s", data)
 }
 
 func main() {
