aea6d608e33a25cab0d13b6b626932ca3615207d
Hank Chou <hank@sharesies.co.nz>
2019-07-01 15:08:40 +1200

    add scraping function in go app

--- main.go
+++ main.go
@@ -2,41 +2,26 @@ package main
 
 import (
 	"fmt"
-	"time"
 
+	"github.com/gocolly/colly"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-
-	"github.com/gin-gonic/gin"
 )
 
-var db *gorm.DB
-var err error
-
-type BitcoinPrice struct {
-	gorm.Model
-	ID      int       `json: "id"`
-	Price   string    `json: "price"`
-	Created time.Time `json: "created"`
+func fetchBitcoinPrice() {
+	c := colly.NewCollector()
+	c.OnHTML("#id-bitcoin", func(e *colly.HTMLElement) {
+		price := e.ChildText(".market-cap")
+		fmt.Println(price)
+	})
+	c.Visit("https://coinmarketcap.com/all/views/all/")
 }
 
 func main() {
-	db, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres password=password dbname=app_db sslmode=disable")
+	db, err := gorm.Open("postgres", "host=localhost user=postgres port=5432 dbname=tool_db")
 	if err != nil {
-		panic("failed to connect the DB")
+		fmt.Printf("failed")
 	}
 	defer db.Close()
-	r := gin.Default()
-	r.GET("/bitcoin", GetBitcoinPrice)
-	r.Run()
-}
-
-func GetBitcoinPrice(c *gin.Context) {
-	var price BitcoinPrice
-	if err := db.Find(price).Error; err != nil {
-		c.AbortWithStatus(404)
-		fmt.Println(err)
-	} else {
-		c.JSON(200, price)
-	}
+	fetchBitcoinPrice()
 }
