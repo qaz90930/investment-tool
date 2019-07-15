a3029216fb4ab8c49985456706b234f5e268b93b
Hank Chou <hank@sharesies.co.nz>
2019-07-15 16:25:08 +1200

    add new fetch

--- app/app.go
+++ app/app.go
@@ -7,30 +7,22 @@ import (
 	"strings"
 	"time"
 
-	"github.com/carlescere/scheduler"
-
 	"github.com/gocolly/colly"
 	"github.com/labstack/echo"
-	"github.com/shopspring/decimal"
 )
 
-type CryptoPrice struct {
-	ID      uint
-	Name    string
-	Price   decimal.Decimal
-	Created time.Time
-}
-
 func showCryptoPrice(c echo.Context) error {
-	scheduler.Every(5).Seconds().Run(fetchBitcoinPrice)
+	// scheduler.Every(5).Seconds().Run(fetchBitcoinPrice)
+	// scheduler.Every(5).Seconds().Run(fetchStockInfo)
+	fetchStockInfo()
 	return c.String(http.StatusOK, "Crypto Page")
 }
 
 func fetchBitcoinPrice() {
 	c := colly.NewCollector()
 	c.OnHTML("#id-bitcoin", func(e *colly.HTMLElement) {
-		rarPrice := e.ChildText(".price")
-		price := strings.Trim(rarPrice, "$")
+		rawPrice := e.ChildText(".price")
+		price := strings.Trim(rawPrice, "$")
 		fmt.Println(price)
 		connStr := "postgres://hank:password@localhost:5432/tool_db?sslmode=disable"
 		db, err := sql.Open("postgres", connStr)
@@ -43,11 +35,22 @@ func fetchBitcoinPrice() {
 	c.Visit("https://coinmarketcap.com/all/views/all/")
 }
 
+func fetchStockInfo() {
+	c := colly.NewCollector()
+	c.OnHTML(".ListFundListing__marketPrice__3yFc0", func(e *colly.HTMLElement) {
+		rawPrice := e.ChildText(".NumberElements__dollarValue__1BUEi")
+		// price := strings.Trim(rawPrice, "$")
+		fmt.Println(e.Text, rawPrice)
+		fmt.Println("124")
+	})
+	c.Visit("https://app.sharesies.nz/invest/companies")
+}
+
 func Index() {
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello World")
 	})
-	e.GET("/crypto", showCryptoPrice)
+	e.GET("/price-list", showCryptoPrice)
 	e.Start(":8001")
 }
