e9e99532d84dacd4545a138f2aa7e7ebd34528ea
Hank Chou <hank@sharesies.co.nz>
2019-07-16 12:36:15 +1200

    add new func & update README.md

--- README.md
+++ README.md
@@ -1,21 +1,48 @@
+# Get Started
+
 ## Installation
-1. Install golang
-2. brew install postgres
-3. Create a database called tool_db
-4. brew install golang-migrate
-5. Install golang packages
+
+[Install golang](https://golang.org/dl/)
+
+[Install Postgres SQL with **brew**](https://gist.github.com/ibraheem4/ce5ccd3e4d7a65589ce84f2a3b7c23a3)
+> brew install postgres
+
+Database Configration
+
+```UNIX
+sudo -u postgres createuser user_name // Create a user for the database
+sudo -u postgres createdb tool_db -o user_name // Create a DB for the app
+sudo -u postgres psql tool_db // Go to the DB
+```
+
+Install golang-migrate
+
+> brew install golang-migrate
+
+Create schema & tables for the app
+
+> migrate -source file://migrations -database "postgres://hank:password@localhost:5432/tool_db?sslmode=disable" up 2
+
+Install golang packages
+
+> go get package_name
 
 ## Run
-* start app 
+
+* start app
+
 > go run main.go // localhost:8001
 
 * run in development  
-> realize start --path="." // dev-tool: localhost:5001 
+
+> realize start --path="." // dev-tool: localhost:5001
 
 ## Migration
+
 > migrate -source file://migrations -database "postgres://hank:password@localhost:5432/tool_db?sslmode=disable" up 2
 
 ## Packages
+
 1. [database/sql](https://golang.org/pkg/database/sql/)
 2. [golang-migrate](https://github.com/golang-migrate/migrate)
 3. [golang-realize](https://github.com/oxequa/realize)
--- app/app.go
+++ app/app.go
@@ -3,6 +3,7 @@ package app
 import (
 	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
 	"strings"
 	"time"
@@ -12,24 +13,29 @@ import (
 	"github.com/labstack/echo"
 )
 
+var connStr = "postgres://hank:password@localhost:5432/tool_db?sslmode=disable"
+
+func landingPage(c echo.Context) error {
+	return c.String(http.StatusOK, "Landing Page")
+}
+
 func showCryptoPrice(c echo.Context) error {
-	scheduler.Every(5).Seconds().Run(fetchBitcoinPrice)
+	scheduler.Every(1).Minutes().Run(fetchBitcoinPrice)
 	// scheduler.Every(5).Seconds().Run(fetchStockInfo)
-	fetchStockInfo()
+	scheduler.Every(1).Minutes().Run(fetchStockInfo)
 	return c.String(http.StatusOK, "Price list")
 }
 
 func fetchBitcoinPrice() {
 	c := colly.NewCollector()
 	c.OnHTML("#id-bitcoin", func(e *colly.HTMLElement) {
-		rawPrice := e.ChildText(".price")
-		price := strings.Trim(rawPrice, "$")
-		fmt.Println(price)
-		connStr := "postgres://hank:password@localhost:5432/tool_db?sslmode=disable"
+		rawData := e.ChildText(".price")
+		price := strings.Trim(rawData, "$")
+		fmt.Println("bitcoin", price)
 		db, err := sql.Open("postgres", connStr)
 		db.QueryRow(`INSERT INTO fund (fund_name, price, created) VALUES ($1, $2, $3)`, "bitcoin", price, time.Now())
 		if err != nil {
-			fmt.Println(err)
+			log.Fatal(err)
 		}
 		defer db.Close()
 	})
@@ -38,20 +44,24 @@ func fetchBitcoinPrice() {
 
 func fetchStockInfo() {
 	c := colly.NewCollector()
-	c.OnHTML(".ListFundListing__marketPrice__3yFc0", func(e *colly.HTMLElement) {
-		rawPrice := e.ChildText(".NumberElements__dollarValue__1BUEi")
-		// price := strings.Trim(rawPrice, "$")
-		fmt.Println(e.Text, rawPrice)
-		fmt.Println("124")
+	c.OnHTML(".small-12.medium-5.columns", func(e *colly.HTMLElement) {
+		rawData := e.ChildText("h1")
+		price := strings.Trim(rawData, "$")
+		fmt.Println("air new zealand", price)
+		db, err := sql.Open("postgres", connStr)
+		db.QueryRow(`INSERT INTO fund (fund_name, price, created) VALUES ($1, $2, $3)`, "Air New Zealand", price, time.Now())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer db.Close()
 	})
-	c.Visit("https://app.sharesies.nz/invest/companies")
+	c.Visit("https://www.nzx.com/instruments/AIR")
 }
 
 func Index() {
+	// This function contains all the routes
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World")
-	})
+	e.GET("/", landingPage)
 	e.GET("/price-list", showCryptoPrice)
 	e.Start(":8001")
 }
